9aebe5ccc30e77017ee71b9bfd8f03b49cf858e1
Moghany <m.osama.elmoghany@gmail.com>
2024-06-22 10:31:35 +0300

    first commit

---
+++ terraform/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "ml_instance" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Choose an appropriate AMI for your region
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "MLInstance"
+  }
+  
+  key_name = "your-key-pair"  # Ensure you have a key pair for SSH access
+
+  provisioner "local-exec" {
+    command = "ansible-playbook -i '${self.public_ip},' -u ec2-user --private-key your-key.pem setup.yml"
+  }
+}
