d46ea67d3c3f3c54289fa60b88e8817cbc6774db
Moghany <m.osama.elmoghany@gmail.com>
2024-06-23 22:57:39 +0300

    terraform

--- infra/setup/main.tf
+++ infra/setup/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-east-1"
+  region  = var.region
   
   default_tags {
     tags = {
@@ -20,20 +20,17 @@ provider "aws" {
   }
 }
 
-resource "aws_s3_bucket" "tf_phi3_s3_storage"{
-  bucket = "tf_phi3_s3_storage_name"
+resource "aws_s3_bucket" "phi3_storage"{
+  bucket = "tf_phi3_s3_tf_state"
+  acl = "public-read"
+
   versioning {
     enabled = true
   }
-
 }
 
-resource "aws_instance" "tf_phi3_ec2_server" {
-  ami           = "ami-830c94e3"
+resource "aws_instance" "tf_phi3_ubuntu_ec2_server" {
+  ami           = "ami-0cf2b4e024cdb6960"
   instance_type = "t2.micro"
-
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
 }
 
--- infra/setup/variables.tf
+++ infra/setup/variables.tf
@@ -6,3 +6,7 @@ variable "contact" {
   description = "contact name for tagging resources"
   default = "m.osama.elmoghany@gmail.com"
 }
+variable "region" {
+  description = "region name for tagging resources"
+  default = "us-east-1"
+}
